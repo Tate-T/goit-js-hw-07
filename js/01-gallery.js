85d674886884145e5a26436ac7866007a5c79a82
Tate-T <tatjana.tarasovych@gmail.com>
2021-09-28 20:52:20 +0300

    task-01 and 02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,52 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+    gallery: document.querySelector('.js-gallery'),
+    openModalWindow: document.querySelector('.js-lightbox'),
+    closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
+    modalWindow: document.querySelector('.lightbox__content'),
+    bigImg: document.querySelector('.lightbox__image'),
+  }
+  
+  const listItemsMarkup = createListMarkup(galleryItems);
+  
+  refs.gallery.insertAdjacentHTML("beforeend", listItemsMarkup);
+  
+  function createListMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+      <a 
+        class="gallery__link" 
+        href = "${original}">
+          <img 
+          class="gallery__image" 
+          src="${preview}" 
+          data-source="${original}" 
+          alt="${description}"
+          />
+      </a>
+    </li>
+    `
+    }).join('');
+  }
+
+  refs.gallery.addEventListener('click', onPictureClick);
+
+function onPictureClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return
+  }
+  refs.openModalWindow.classList.add('is-open');
+  refs.bigImg.src = e.target.dataset.source;
+}
+
+refs.openModalWindow.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal(e) {
+  refs.openModalWindow.classList.toggle('is-open');
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,56 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+window.addEventListener('keydown', onEscBtnPress);
+window.addEventListener('keydown', onRightLeftBtn);
+
+function onEscBtnPress(e) {
+  if (e.code !== 'Escape') {
+    return;
+  }
+  refs.openModalWindow.classList.remove('is-open');
+  refs.bigImg.setAttribute('src', "");
+}
+
+function onRightLeftBtn(e) {
+  if (e.code === "ArrowRight") {
+    const imgArrey = refs.gallery.querySelectorAll(".gallery__image");
+
+    const srcArrey = [...imgArrey].map((src) => src.dataset.source);
+
+    const currentImgHref = refs.bigImg.getAttribute("src");
+
+    let nextImgHref = "";
+    for (let i = 0; i < srcArrey.length; i += 1) {
+      const currentNumberHref = srcArrey.indexOf(currentImgHref);
+
+      const nextNumberHref = currentNumberHref +1;
+      if (nextNumberHref >= srcArrey.length) {
+        return;
+      }
+      nextImgHref = srcArrey[nextNumberHref];
+    }
+    refs.bigImg.setAttribute("src", nextImgHref);
+  }
+
+  if (e.code === "ArrowLeft") {
+    const imgArrey = refs.gallery.querySelectorAll(".gallery__image");
+
+    const srcArrey = [...imgArrey].map((src) => src.dataset.source);
+
+    const currentImgHref = refs.bigImg.getAttribute("src");
+
+    let nextImgHref = "";
+    for (let i = 0; i < srcArrey.length; i += 1) {
+      const currentNumberHref = srcArrey.indexOf(currentImgHref);
+
+      const nextNumberHref = currentNumberHref -1;
+      if (nextNumberHref < 0) {
+        return;
+      }
+      nextImgHref = srcArrey[nextNumberHref];
+    }
+    refs.bigImg.setAttribute("src", nextImgHref);
+  }
+}
